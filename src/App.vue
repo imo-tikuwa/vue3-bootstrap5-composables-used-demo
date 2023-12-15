ec810aaa0fd7c7c4630924192f3fc063c090895d
imo-tikuwa <imotikuwabathclean@gmail.com>
2023-12-15 19:34:46 +0900

    add `$composableToast` used demo.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <header class="mt-4 mb-4 text-center">
-      <h1>Vue3&Bootstrap5 Confirm Modal Example.</h1>
+      <h1>Vue3&Bootstrap5 Composable Examples.</h1>
     </header>
 
     <main class="text-center">
@@ -13,6 +13,9 @@
       </div>
       <div class="mt-4">
         <DemoChildComponent3 />
+      </div>
+      <div class="mt-4">
+        <DemoChildComponent4 />
       </div>
     </main>
 
@@ -26,4 +29,5 @@
 import DemoChildComponent1 from './components/DemoChildComponent1.vue'
 import DemoChildComponent2 from './components/DemoChildComponent2.vue'
 import DemoChildComponent3 from './components/DemoChildComponent3.vue'
+import DemoChildComponent4 from './components/DemoChildComponent4.vue'
 </script>
---
+++ src/components/DemoChildComponent4.vue
@@ -0,0 +1,118 @@
+<template>
+  <h2>Demo4 (Use `$composableToast`)</h2>
+  <div class="d-flex flex-column flex-xxl-row align-items-center justify-content-center">
+    <div class="d-flex flex-xxl-fill justify-content-center justify-content-lg-end mb-2 mb-xxl-0">
+      <button
+        type="button"
+        class="btn btn-primary me-2"
+        @click="
+          $composableToast.show({
+            title: 'Basic Toast',
+            content: 'This is Primary Toast.'
+          })
+        "
+      >
+        show
+      </button>
+      <button
+        type="button"
+        class="btn btn-secondary me-2"
+        @click="
+          $composableToast.show({
+            title: 'Basic Toast',
+            content: 'This is Secondary Toast.',
+            theme: 'secondary'
+          })
+        "
+      >
+        show
+      </button>
+      <button
+        type="button"
+        class="btn btn-success me-2"
+        @click="
+          $composableToast.show({
+            title: 'Basic Toast',
+            content: 'This is Success Toast.',
+            theme: 'success'
+          })
+        "
+      >
+        show
+      </button>
+      <button
+        type="button"
+        class="btn btn-danger me-2"
+        @click="
+          $composableToast.show({
+            title: 'Basic Toast',
+            content: 'This is Danger Toast.',
+            theme: 'danger'
+          })
+        "
+      >
+        show
+      </button>
+    </div>
+    <div class="d-flex flex-xxl-fill justify-content-center justify-content-lg-start">
+      <button
+        type="button"
+        class="btn btn-warning me-2"
+        @click="
+          $composableToast.show({
+            content: 'This is Simple Warning Toast.',
+            theme: 'warning',
+            mode: 'simple',
+            group: 'another-toast-container'
+          })
+        "
+      >
+        show
+      </button>
+      <button
+        type="button"
+        class="btn btn-info me-2"
+        @click="
+          $composableToast.show({
+            content: 'This is Simple Info Toast.',
+            theme: 'info',
+            mode: 'simple',
+            group: 'another-toast-container'
+          })
+        "
+      >
+        show
+      </button>
+      <button
+        type="button"
+        class="btn btn-light me-2"
+        @click="
+          $composableToast.show({
+            content: 'This is Simple Light Toast.',
+            theme: 'light',
+            mode: 'simple',
+            group: 'another-toast-container'
+          })
+        "
+      >
+        show
+      </button>
+      <button
+        type="button"
+        class="btn btn-dark me-2"
+        @click="
+          $composableToast.show({
+            content: 'This is Simple Dark Toast.',
+            theme: 'dark',
+            mode: 'simple',
+            group: 'another-toast-container'
+          })
+        "
+      >
+        show
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
